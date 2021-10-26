23901e0b38e501b30ecc90940fdbb550285be655
srihariesh <sriharieshkg.20cse@kongu.edu>
2021-10-26 19:13:59 +0530

    placementcommit

---
+++ placedetail.java
@@ -0,0 +1,11 @@
+	Student Name: String
+Rollno: String
+Mobile Number: Integer
+Personal Mailid: String
+Edu Mailid: String
+Days Scholar/ Hosteller: Integer
+Number of Arrears: Integer
+GPA (Upto 1 st Sem): Integer
+GPA (Upto 2 nd Sem): Integer
+GPA (Upto 3 rd Sem): Integer
+CGPA (Upto 3 rd Sem): Integer
