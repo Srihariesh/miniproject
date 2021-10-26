471ca0d878f0b10896c03a0a9a35813238980d83
srihariesh <sriharieshkg.20cse@kongu.edu>
2021-10-26 19:09:13 +0530

    firstcommit

---
+++ details.java
@@ -0,0 +1,10 @@
+Name: String
+Rollno: String
+Father Name: String
+Mother Name: String
+Age: Integer
+Mobile Number: Integer
+Email: String
+Blood Group: Integer
+Address: String
+Aadhar Number: Integer
