b418a82d9c40d7db9e8dc4f751c771f6673b1422
srihariesh <sriharieshkg.20cse@kongu.edu>
2021-10-26 19:12:04 +0530

    insurencecommit

---
+++ insdetail.java
@@ -0,0 +1,9 @@
+Student Name: String
+Nominee Name: String
+
+Relationship of Nominee: String
+Nominee Age: Integer
+Nominee Mobile Number: Integer
+Nominee Occupation: String
+Nominee Email: String
+Nominee Aadhar Number: Integer
