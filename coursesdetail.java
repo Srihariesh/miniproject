2a714f493eed95bc29668abdc5a90fd71bb19c48
srihariesh <sriharieshkg.20cse@kongu.edu>
2021-10-26 19:31:25 +0530

    coursescomplete

---
+++ coursesdetail.java
@@ -0,0 +1,6 @@
+Rollno: String
+Semester: Number
+No of One credit courses completed: Integer
+Name of One credit courses completed: String Array
+No of value added courses completed: Integer
+Name of value added courses: String Array
