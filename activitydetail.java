eb76d85a4e6d8b5b7fde6cbff26e1f4aa41a89ed
srihariesh <sriharieshkg.20cse@kongu.edu>
2021-10-26 19:27:10 +0530

    activitycommit

---
+++ activitydetail.java
@@ -0,0 +1,8 @@
+Rollno: String
+Semester: Number
+No. of papers presented inside KEC: Integer
+No. of projects presented inside KEC: Integer
+No. of papers presented outside KEC: Integer
+No. of projects presented outside KEC: Integer
+No. of prize won in Coding Carnival: Integer
+No. of prize won in Hackathon: Integer
